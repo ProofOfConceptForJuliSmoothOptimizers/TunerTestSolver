45d1876ab0735cca5195edbc244529f2c74627a0
tmigot <tmigot@users.noreply.github.com>
2023-05-25 17:10:59 +0000

    :robot: Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,31 @@
 using TunerTestSolver
 using Documenter
 
-DocMeta.setdocmeta!(TunerTestSolver, :DocTestSetup, :(using TunerTestSolver); recursive = true)
+DocMeta.setdocmeta!(
+    TunerTestSolver,
+    :DocTestSetup,
+    :(using TunerTestSolver);
+    recursive = true,
+)
 
 makedocs(;
-  modules = [TunerTestSolver],
-  doctest = true,
-  linkcheck = false,
-  strict = false,
-  authors = "Tangi Migot and Monssaf Toukal",
-  repo = "https://github.com/ProofOfConceptForJuliSmoothOptimizers/TunerTestSolver.jl/blob/{commit}{path}#{line}",
-  sitename = "TunerTestSolver.jl",
-  format = Documenter.HTML(;
-    prettyurls = get(ENV, "CI", "false") == "true",
-    canonical = "https://ProofOfConceptForJuliSmoothOptimizers.github.io/TunerTestSolver.jl",
-    assets = ["assets/style.css"],
-  ),
-  pages = ["Home" => "index.md", "Reference" => "reference.md"],
+    modules = [TunerTestSolver],
+    doctest = true,
+    linkcheck = false,
+    strict = false,
+    authors = "Tangi Migot and Monssaf Toukal",
+    repo = "https://github.com/ProofOfConceptForJuliSmoothOptimizers/TunerTestSolver.jl/blob/{commit}{path}#{line}",
+    sitename = "TunerTestSolver.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://ProofOfConceptForJuliSmoothOptimizers.github.io/TunerTestSolver.jl",
+        assets = ["assets/style.css"],
+    ),
+    pages = ["Home" => "index.md", "Reference" => "reference.md"],
 )
 
 deploydocs(;
-  repo = "github.com/ProofOfConceptForJuliSmoothOptimizers/TunerTestSolver.jl",
-  push_preview = true,
-  devbranch = "main",
+    repo = "github.com/ProofOfConceptForJuliSmoothOptimizers/TunerTestSolver.jl",
+    push_preview = true,
+    devbranch = "main",
 )
